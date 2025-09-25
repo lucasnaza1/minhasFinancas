49235066e9d48b08d448c0447521bc4373bdfa6a
Lucas Nazário <lucaskenpachi57@gmail.com>
2025-09-25 20:55:07 -0300

    Projeto finalizado!

--- src/Main.java
+++ src/Main.java
@@ -4,48 +4,46 @@ public class Main {
         String nome = "Lucas Nazário";
         String tipoConta = "Conta Corrente";
         double saldo = 2000.00;
-
+        System.out.println("-------------------------------------------------------");
         System.out.println("MINHAS FINANÇAS \n" +
                 " Nome: " + nome + "\n" +
-                " Tipo de conta: " + tipoConta + "\n"+
+                " Tipo de conta: " + tipoConta + "\n" +
                 " Saldo: " + saldo);
-
+        System.out.println("-------------------------------------------------------");
         Scanner insereOpcao = new Scanner(System.in);
         int opcao = 0;
         double saque = 0;
         double deposito = 0;
 
         String menu = """
-                  1 - Verificar Saldo
-                  2 - Sacar
-                  3 - Depositar
-                  4 - Crédito
-                  5 - Sair
-               """;
+                   1 - Verificar Saldo
+                   2 - Sacar
+                   3 - Depositar
+                   4 - Sair
+                   Escolha a opção:
+                """;
 
-        while (opcao != 5) {
+        while (opcao != 4) {
             System.out.println(menu);
             opcao = insereOpcao.nextInt();
-            if (opcao == 1){
-                System.out.println(saldo);
-            } else if(opcao == 2) {
+            if (opcao == 1) {
+                System.out.println("Saldo Atual: " + saldo);
+            } else if (opcao == 2) {
                 System.out.println("Digite o valor a ser sacado:");
-                Scanner sacarDinheiro = new Scanner(System.in);
-                saque = sacarDinheiro.nextDouble();
-                if(saque <= saldo){
-                    saldo = saldo - saque;
-                    System.out.println(saldo);
+                saque = insereOpcao.nextDouble();
+                if (saque <= saldo) {
+                    saldo -= saque;
+                    System.out.println("Saldo Atual: " + saldo);
                 } else {
                     System.out.println("Saldo Insuficiente!");
                 }
-        } else if (opcao == 3){
+            } else if (opcao == 3) {
                 System.out.println("Digite o valor a ser inserido:");
-                Scanner depositarSaldo =  new Scanner(System.in);
-                deposito = depositarSaldo.nextDouble();
-                saldo = deposito + saldo;
-                System.out.println(saldo);
-            } else {
-                System.out.println("Em breve");
+                deposito = insereOpcao.nextDouble();
+                saldo += deposito;
+                System.out.println("Saldo Insuficiente!");
+            } else if (opcao != 4) {
+                System.out.println("Opção Inválida!");
             }
         }
     }
